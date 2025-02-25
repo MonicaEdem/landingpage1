b208c126296993d5a95222caa007d3eece02782f
Monica Edem Kokovena <monicaekokovena@gmail.com>
2025-02-25 21:39:57 +0000

    testing api

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const fetchData = async (range) => {
     const url = import.meta.env.VITE_BASE_URL;
+    console.log("VITE_BASE_URL (Netlify):", import.meta.env.VITE_BASE_URL);
     try {
       const response = await axios.get(url);
       return response.data.values; 
